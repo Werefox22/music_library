56bbfd22b828a53f76dc8638b63553ac638b9377
Werefox <jack@wrightus.com>
2022-06-13 20:00:15 -0400

    Removed the search state variable since it was no longer needed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 // Components
 import Gallery from './components/Gallery';
@@ -8,22 +8,23 @@ import { DataContext } from './context/DataContext';
 import { SearchContext } from './context/SearchContext';
 
 function App() {
-	let [search, setSearch] = useState("");
 	let [data, setData] = useState([]);
 	let [message, setMessage] = useState("Search for music!");
 
 	let searchInput = useRef('')
 
-	useEffect(() => {
+	const handleSearch = (e, term) => {
+		e.preventDefault()
+
 		const fetchData = async () => {
-			if (search) {
-				document.title = `${search} Music`
-				const response = await fetch(`https://itunes.apple.com/search?term=` + search)
+			if (term) {
+				document.title = `${term} Music`
+				const response = await fetch(`https://itunes.apple.com/search?term=` + term)
 				const resData = await response.json()
 				
 				if (resData.results.length > 0) {
 					setData(resData.results)
-					console.log(resData.results)
+					// console.log(resData.results)
 				} else {
 					setMessage('Not found')
 				}
@@ -31,11 +32,6 @@ function App() {
 		}
 
 		fetchData()
-	}, [search])
-
-	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
 	}
 
 	return (
