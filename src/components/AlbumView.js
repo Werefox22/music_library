f73cd20f47cefdd532b14075b3de6354bff7bb33
Werefox <jack@wrightus.com>
2022-06-15 18:40:52 -0400

    Added data fetching and artist links to the album view

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 function AlbumView() {
 	const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter((entry) => entry.wrapperType === 'track')
+
+    const renderAlbum = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <Link to={`/artist/${song.artistId}`} >
+                    <p>{song.trackName}</p>
+                </Link>
+            </div>
+        )
+    })
+
     return (
         <div>
             <h1>AlbumView</h1>
 			<p>Id: {id}</p>
+            {renderAlbum}
         </div>
     )
 }
