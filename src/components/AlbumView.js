26da881e7d4983d79cd35abcd488cc8f2fdfb67e
Werefox <jack@wrightus.com>
2022-06-15 12:16:40 -0400

    Stubbed Album and Arist view components

---
+++ src/components/AlbumView.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function AlbumView() {
+    const [ albumData, setAlbumData ] = useState([])
+
+    return (
+        <div>
+            AlbumView
+        </div>
+    )
+}
+
+export default AlbumView
---
+++ src/components/ArtistView.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function ArtistView() {
+    const [ artistData, setArtistData ] = useState([])
+
+    return (
+        <div>
+            ArtistView
+        </div>
+    )
+}
+
+export default ArtistView
