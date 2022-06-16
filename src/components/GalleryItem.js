5485899d75d6a04a0390242d32e0985bfc5ea0df
Werefox <jack@wrightus.com>
2022-06-16 16:50:59 -0400

    Styled the search bar and the gallery items

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,40 +1,64 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom'
 
+// Styling
+import Card from "react-bootstrap/Card";
+
+
 function GalleryItem(props) {
 	let [view, setView] = useState(false);
 
 	const simpleView = () => {
 		return (
-			<div>
-				<img src={props.item.artworkUrl60} alt={props.item.collectionName}/>
-				<h4>{props.item.trackName}</h4>
-			</div>
+			<Card style={{width: '10rem'}}>
+				<Card.Img variant='top' src={props.item.artworkUrl100} alt={props.item.collectionName}/>
+				<Card.Body>
+					<Card.Title>{props.item.trackName}</Card.Title>
+					<Card.Subtitle>{props.item.artistName}</Card.Subtitle>
+				</Card.Body>
+			</Card>
 		)
 	}
 
 	const detailedView = () => {
 		return (
-			<div>
-				<img src={props.item.artworkUrl100} alt={props.item.collectionName}/>
-				<h4>
-					<Link to={`/song/${props.item.trackId}`}>
-						{props.item.trackName}
-					</Link>
-				</h4>
+			<Card style={{width: '12rem'}}>
+				<Card.Img variant='top' src={props.item.artworkUrl100} alt={props.item.collectionName}/>
+				<Card.Body>
+					<Card.Title>
+						<Link to={`/song/${props.item.trackId}`}>{props.item.trackName}</Link>
+					</Card.Title>
+					<Card.Subtitle>
+						<Link to={`/artist/${props.item.artistId}`}>{props.item.artistName}</Link>
+					</Card.Subtitle>
+					<Card.Text>
+						<hr />
+						<Link to={`/album/${props.item.collectionId}`}>
+							{`Track ${props.item.trackNumber}/${props.item.trackCount} from ${props.item.collectionName}`}
+						</Link>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+			// <div>
+			// 	<img src={props.item.artworkUrl100} alt={props.item.collectionName}/>
+			// 	<h4>
+			// 		<Link to={`/song/${props.item.trackId}`}>
+			// 			{props.item.trackName}
+			// 		</Link>
+			// 	</h4>
 
-				<h5>
-					<Link to={`/artist/${props.item.artistId}`}>
-						{props.item.artistName}
-					</Link>
-				</h5>
-				<h5>
-					<Link to={`/album/${props.item.collectionId}`}>
-						{props.item.collectionName}
-					</Link>
-				</h5>
+			// 	<h5>
+			// 		<Link to={`/artist/${props.item.artistId}`}>
+			// 			{props.item.artistName}
+			// 		</Link>
+			// 	</h5>
+			// 	<h5>
+			// 		<Link to={`/album/${props.item.collectionId}`}>
+			// 			{props.item.collectionName}
+			// 		</Link>
+			// 	</h5>
 
-			</div>
+			// </div>
 		)
 	}
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,29 @@
 import React, { useContext } from "react";
 import { SearchContext } from "../context/SearchContext";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
 
 function SearchBar() {
 	let {term, handleSearch} = useContext(SearchContext);
 
 	return (
-		<form>
-			<input 
-				type='text' 
-				placeholder="Search for your favorite song..." 
-				ref={term}
-			/>
-			<button onClick={(e) => {handleSearch(e, term.current.value)}}>Submit</button>
-		</form>
+		<Form>
+			<Form.Group controlId="search">
+				<InputGroup>
+					<Form.Control placeholder='Search for your favorite song' ref={term}/>
+					<Button 
+						variant='primary'
+						type='button'
+						onClick={(e) => {handleSearch(e, term.current.value)}}
+					>
+						Submit
+					</Button>
+
+				</InputGroup>
+			</Form.Group>
+
+		</Form>
 	)
 }
 
